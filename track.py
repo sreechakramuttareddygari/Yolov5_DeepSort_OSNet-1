5985a0ffa7d62dfdb2178a52a50942d6ad531c4c
sreechakramuttareddygari <49017747+sreechakramuttareddygari@users.noreply.github.com>
2022-04-29 01:07:38 +0530

    Update track.py

--- track.py
+++ track.py
@@ -173,7 +173,9 @@ def detect(opt):
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
-
+                
+                print(det)
+                
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
